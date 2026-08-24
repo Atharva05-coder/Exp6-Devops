50e2e67eda6d5e2bf44654af53364c88d6149b6a
Atharva <atharvaamrutkar5@gmail.com>
2026-08-24 11:50:09 +0530

    Revert "Merge branch 'main' of https://github.com/Atharva05-coder/Exp6-Devops"
    This reverts commit eee3612bff63a04c29635d7cf81f8b6cf665d7bb, reversing
    changes made to e62c3d52efbdce767448338b961c0935e6501f87.

--- atharva/src/test/java/Atharva/AppTest.java
+++ atharva/src/test/java/Atharva/AppTest.java
@@ -1,40 +1,19 @@
 package Atharva;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class App {
+import org.junit.jupiter.api.Test;
 
-    public static void main(String[] args) throws InterruptedException {
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
 
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        Thread.sleep(3000);
-        driver.get("https://the-internet.herokuapp.com/login");
-        Thread.sleep(3000);
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        Thread.sleep(3000);
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        Thread.sleep(3000);
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
-
-        Thread.sleep(3000);
-
-        String message = driver.findElement(By.id("flash")).getText();
-
-        if(message.contains("You logged into a secure area!"))
-        {
-            System.out.println("TEST PASSED");
-        }
-        else
-        {
-            System.out.println("TEST FAILED");
-        }
-
-        Thread.sleep(3000);
-
-       driver.quit();
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
-} 
+}
